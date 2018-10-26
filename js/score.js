0c0008e962a86dd4a80d6d31812ff36d1244af69
zjaredkhan <jared.khan@outlook.com>
2018-10-25 20:11:01 -0700

    added very basic scoring

--- js/score.js
+++ js/score.js
@@ -1,9 +1,14 @@
 'use strict';
 
-var scoreTest = 37
+var scoreTest = 37;
+var testName = 'Yoda';
 
-var score = JSON.parse(localStorage.getItem('moves'));
 
-document.getElementById("High_Scores");
-var scorePass = document.createElement("li");
-scorePass.textContent = scoreTest;
+var score = JSON.parse(localStorage.getItem('moves')); //access local storage for moves
+var name = JSON.parse(localStorage.getItem('name')); // access local storage for player name
+
+var scorePass = document.createElement('li'); //create li node
+var textNode = document.createTextNode(testName + ' - ' + scoreTest); //create text node
+scorePass.appendChild(textNode); // append score to li
+
+document.getElementById('High_Scores').appendChild(scorePass);
