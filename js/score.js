0a769b201c96e6b9f58b16ed6f02eed580c4b7c5
zjaredkhan <jared.khan@outlook.com>
2018-10-25 20:56:25 -0700

    varied basic local storage working

--- js/score.js
+++ js/score.js
@@ -9,9 +9,10 @@ var score = JSON.parse(localStorage.getItem('moves')); //access local storage fo
 var name = JSON.parse(localStorage.getItem('name')); // access local storage for player name
 
 var scorePass = document.createElement('li'); //create li node
-var textNode = document.createTextNode(testName + ' - ' + scoreTest); //create text node
+var textNode = document.createTextNode(testName + ' - ' + score); //create text node
 scorePass.appendChild(textNode); // append score to li
 
 document.getElementById('High_Scores').appendChild(scorePass);
 
 // thoughts - may need to put the score(s) and name(s) into an array and loop them
+
