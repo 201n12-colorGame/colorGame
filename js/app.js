bd2e5495887c3eea6b62748e19bfd766dc60319d
zjaredkhan <jared.khan@outlook.com>
2018-10-29 19:56:12 -0700

    worked on JS

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ var size = 5;
 //initialize board state. for now, the entire board is initialized as zeroes.
 var initializeBoard = function (num) {
   size = num;
-  for (var i = 0; i < num; i++) {
+  for (var row = 0; row < num; row++) {
     colorBoard.push([]);
-    for (var j = 0; j < num; j++) {
-      colorBoard[i].push(0);
+    for (var col = 0; col < num; col++) {
+      colorBoard[row].push(0);
     }
-  }
+  }localStorage.getItem('winnerArray');
   console.log(colorBoard);
   renderBoard();
 };
@@ -25,17 +25,17 @@ var renderBoard = function () {
     boardHolder.removeChild(boardHolder.firstChild);
   }
 
-  //i iterates through table rows.
-  for (var i = 0; i < size; i++) {
+  //row iterates through table rows.
+  for (var row = 0; row < size; row++) {
     var trEl = document.createElement('tr');
     boardHolder.appendChild(trEl);
 
-    //j iterates through elements/columns.
-    for (var j = 0; j < size; j++) {
+    //col iterates through elements/columns.
+    for (var col = 0; col < size; col++) {
       var tdEl = document.createElement('td');
-      tdEl.className = 'style' + colorBoard[i][j];
+      tdEl.className = 'style' + colorBoard[row][col];
       //we assign the content of the board as a class name so that its appearance can be manipulated in CSS
-      tdEl.id = i + ' ' + j;
+      tdEl.id = row + ' ' + col;
       // each box is named for its row and column for ease of manipulation
       tdEl.addEventListener('click', tileClickHandler);
       trEl.appendChild(tdEl);
@@ -47,29 +47,78 @@ var renderBoard = function () {
 var tileClickHandler = function (eventObject) {
   //this is where the magic happens!
 
-  //increment moves and write to local storage
-  moves++;
-  localStorage.setItem('moves', moves);
-
   //pull out the coordinates of the clicked box
   var coord = eventObject.target.id.split(' ');
   coord[0] = parseInt(coord[0]);
   coord[1] = parseInt(coord[1]);
 
   //set the identities of clicked tile and adjacent tiles in memory
-  for(var i = coord[0]-1; i < coord[0]+2; i++){
-    for(var j = coord[1]-1; j< coord[1]+2; j++){
-      if(j > -1 && i > -1 && j < size && i < size){
-        if(colorBoard[i][j]){
-          colorBoard[i][j] = 0;
+  for(var row = coord[0]-1; row < coord[0]+2; row++){
+    for(var col = coord[1]-1; col< coord[1]+2; col++){
+      if(col > -1 && row > -1 && col < size && row < size){
+        if(colorBoard[row][col]){
+          colorBoard[row][col] = 0;
         } else {
-          colorBoard[i][j] = 1;
+          colorBoard[row][col] = 1;
         }
       }
     }
   }
   renderBoard();
-console.log(coord);
+  if (checkWin()){
+    winHandler();
+  }
+  // console.log(coord);
 };
+
+// randomizes the board 
+var randomize = function(){
+  
+}
+
+var winHandler = function(){
+  var name = prompt('Congratulations you won! Please enter your name');
+  var winnerArray = JSON.parse(localStorage.getItem('winnerArray'));
+
+  console.log(winnerArray);
+  if (winnerArray === null){
+    winnerArray = [];
+  }
+  winnerArray.push([name, moves]);
+  localStorage.setItem('winnerArray', winnerArray);
+};
+
+
+// check if game is won
+var checkWin = function(){
+  var cointainsOne = 0;
+  var containsZero = 0;
+  var row = 0;
+  var col = 0;
+
+  while (!(containsZero && cointainsOne) && row < size){
+    if (colorBoard[row][col]){
+      cointainsOne = 1;
+    }
+    else{
+      containsZero = 1;
+    }
+    col++;
+    if (col === size){
+      col = 0; row++;
+    }
+    // console.log(row + ' ' + col + ' ' + colorBoard[row][col]);
+  }
+
+  return (row === size);
+
+  // if(!((colorBoard.findIndex(0) + 1) && (colorBoard.findIndex(1) + 1))){
+  //   var name = prompt('Congratulations you win! What is your name?');
+  //}
+}
+
+//increment moves and write to local storage
+moves++;
+localStorage.setItem('moves', moves);
 
 initializeBoard(9);
